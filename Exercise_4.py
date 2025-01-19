a142692583207a411657902afda4f20d690eabba
Ranga sai <yalakaranga@gmail.com>
2025-01-18 19:16:38 -0500

    precourse 1

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,38 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+  if len(arr)<=1:
+    return arr
   
+  left =0
+  right = len(arr)
+  mid = (left+right)//2
+
+  left_array=mergeSort(arr[:mid])
+  right_array = mergeSort(arr[mid:])
+
+  i,j=0,0
+  result =[]
+  while i < len(left_array) and j < len(right_array):
+    if left_array[i] < right_array[j]:
+        result.append(left_array[i])
+        i += 1
+    else:
+        result.append(right_array[j])
+        j += 1
+
+  result.extend(left_array[i:])
+  result.extend(right_array[j:])
+  
+  return result
+
+  
+
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
-    
+    print(arr)
+    return
     #write your code here
   
 # driver code to test the above code 
@@ -13,6 +40,6 @@ if __name__ == '__main__':
     arr = [12, 11, 13, 5, 6, 7]  
     print ("Given array is", end="\n")  
     printList(arr) 
-    mergeSort(arr) 
+    result=mergeSort(arr) 
     print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    printList(result) 
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,27 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
-
-
+    pivot = arr[h]
+    i = l - 1  
+    for j in range(l, h):
+        if arr[j] <= pivot: 
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i] 
+    
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    
+    return i + 1 
 def quickSortIterative(arr, l, h):
-  #write your code here
+    stack = []
+    stack.append((l, h))
+    
+    while stack:
+        l, h = stack.pop()
+        
+        if l < h:
+            pivot_index = partition(arr, l, h)
+            
+            stack.append((l, pivot_index - 1))
+            
+            stack.append((pivot_index + 1, h))
 
